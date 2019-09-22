a147e6c524cb1a68324c4e00b9de48b25971622c
João Teodoro <joaotdn@gmail.com>
2019-09-21 22:29:33 -0300

    desenvolvendo seção inicial - poster

---
+++ src/assets/js/lib/preloadConfig.js
@@ -0,0 +1,13 @@
+var QueryLoader2 = require("queryloader2");
+
+var loader = QueryLoader2(document.querySelector("body"), {
+    barColor: "#00156B",
+    backgroundColor: "#fff",
+    percentage: false,
+    barHeight: 1,
+    minimumTime: 200,
+    fadeOutTime: 1000,
+    onComplete() {
+        console.log('teste');
+    }
+});
